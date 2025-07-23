4b1a3945a2aa8daeb9c20b42cd58638b76c0b303
IsmaelEnrique <87545887+IsmaelEnrique@users.noreply.github.com>
2025-07-23 19:05:46 -0300

    Delete Tp-Final directory

--- Tp-Final/backend-express-sqlite/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app'); // importar la configuración de la app
-const port = 3001;
-
-app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-});
